3f2c140f235850981ac4b78d29b353e912501032
Simon Plourde <splourde@nuglif.com>
2015-12-06 17:39:16 -0500

    Use drone as CI pipeline

--- attributes/default.rb
+++
@@ -1,6 +0,0 @@
-default['uchiwa-build']['workdir'] = '/tmp/build'
-default['uchiwa-build']['golang'] = 'https://storage.googleapis.com/golang/go1.3.3.linux-amd64.tar.gz'
-default['uchiwa-build']['git'] = 'github.com/sensu/uchiwa'
-
-default['uchiwa-build']['uchiwa_version'] = '0.0.1'
-default['uchiwa-build']['build_number'] = '1'
--- metadata.rb
+++
@@ -1,9 +0,0 @@
-name                'uchiwa-build'
-maintainer          'Simon Plourde'
-maintainer_email    'simon.plourde@gmail.com'
-license             'MIT'
-description         'Builds/Tests Uchiwa packages'
-long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version             '0.0.1'
-
-supports 'centos'
